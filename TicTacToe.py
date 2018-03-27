5c8a91ef2a15aa8316d29a737500445edd86f095
Alisha Rogerson <alisha.rogerson@hotmail.co.uk>
2018-03-27 21:07:16 +0100

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,35 +1,43 @@
-'''
-Tic Tac Toe Milestone Project 1
+'''Tic Tac Toe Milestone Project 1'''
 
+'''
 A board of nested lists, referenced by A-C, 1-3 grid positions
 which will store 'X' and 'O' for player moves and an underscore
 represents an unoccupied position.
 '''
-print ("-- Tic Tac Toe --\n")
-print ("Input the row, them column of the space you would like to take!\nFor example, type A1 to take the top left hand corner,\nor C3 to take the bottom right corner.")
 
-##Values
+# Intro to game & example for userability
+print ("-- Tic Tac Toe --\n")
+print ("Input the row, them column of the space you would like to take!
+print ("For example, type A1 to take the top left hand corner,\nor C3 to take the bottom right corner.")
+
+# Values for counts & storing taken grid positions
 Play_Count = 0
 Taken_Val = []
 Taken_Total = " "
 
-## Grid
+       
+'''Creating the grid'''
+
 grid = []
-for (x) in range(0,3):
+for (x) in range(0,3):  # function to create a 3 x 3 grid
     grid.append(["_"]*3)
-        
-def print_grid(grid):
+   
+def print_grid(grid):  # function to print the grid with rows labelled A, B, C
     ABC = "ABC"
     row = 0
-    for (num) in grid:
+    for (num) in grid:  # for each row (list) in grid - label and join with pipe
          print (((ABC)[(row)])+": |"+"|".join(num))
          row += 1
 
-# Play Function
+
+'''Play function'''
+
 def play():
     global Taken_Total
-    Play = input ("\n\nWhere on the grid would you like to place your marker?: ").lower()
-    #Validating input
+    Play = input("\n\nWhere on the grid would you like to place your marker?: ").lower()
+    
+    # Validating input
     while True:
         if len(Play) != 2:
             Play = input ("Invalid Input. Please try again: ").lower()
@@ -41,11 +49,12 @@ def play():
             Play = input ("Invalid Input. Please try again: ").lower()
         else:
             break
-    #Storing move
+       
+    # Storing move
     Taken_Val.append(Play)
     Taken_Total += ((Play)+(Player))
     
-    #Changing Grid - adding marker
+    # Changing Grid - adding marker
     while True:
         if (Play) == "a1" and grid[0][0] == "_":
             grid[0][0] = (Player)
@@ -77,48 +86,66 @@ def play():
 
 ## Winning Combinations - Taken_Val
 while True:
-    if ("a1O") in (Taken_Total) and("a2O") in (Taken_Total) and("a3O") in (Taken_Total): #A across - O
+    # A across - O
+    if ("a1O") in (Taken_Total) and("a2O") in (Taken_Total) and("a3O") in (Taken_Total):
         break
-    if ("a1X") in (Taken_Total) and ("a2X") in (Taken_Total) and ("a3X") in (Taken_Total): #A across - X
+    # A across - X
+    if ("a1X") in (Taken_Total) and ("a2X") in (Taken_Total) and ("a3X") in (Taken_Total):
         break
-    if ("b1O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("b3O") in (Taken_Total): #B across - O
+    # B across - O
+    if ("b1O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("b3O") in (Taken_Total):
         break
-    if ("b1X") in (Taken_Total) and ("b2X") in (Taken_Total) and("b3X") in (Taken_Total): #B across - X
+    # B across - X
+    if ("b1X") in (Taken_Total) and ("b2X") in (Taken_Total) and("b3X") in (Taken_Total):
         break
-    if ("c1O") in (Taken_Total) and ("c2O") in (Taken_Total) and("c3O") in (Taken_Total): #C across - O
+    # C across - O
+    if ("c1O") in (Taken_Total) and ("c2O") in (Taken_Total) and("c3O") in (Taken_Total):
         break
-    if ("c1X") in (Taken_Total) and ("c2X") in (Taken_Total) and("c3X") in (Taken_Total): #C across - X
+    # C across - X
+    if ("c1X") in (Taken_Total) and ("c2X") in (Taken_Total) and("c3X") in (Taken_Total):
         break
-    if ("a1O") in (Taken_Total) and ("b1O") in (Taken_Total) and ("c1O") in (Taken_Total): #1st Col Down - O
+    # 1st Col Down - O
+    if ("a1O") in (Taken_Total) and ("b1O") in (Taken_Total) and ("c1O") in (Taken_Total):
         break
-    if ("a1X") in (Taken_Total) and ("b1X") in (Taken_Total) and ("c1X") in (Taken_Total): #1st Col Down - X
+    # 1st Col Down - X
+    if ("a1X") in (Taken_Total) and ("b1X") in (Taken_Total) and ("c1X") in (Taken_Total):
         break
-    if ("a2O") in (Taken_Total) and ("b2O") in (Taken_Total) and("c2O") in (Taken_Total): #2nd Col Down - O
+    # 2nd Col Down - O
+    if ("a2O") in (Taken_Total) and ("b2O") in (Taken_Total) and("c2O") in (Taken_Total):
         break
-    if ("a2X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c2X") in (Taken_Total): #2nd Col Down - X
+    # 2nd Col Down - X
+    if ("a2X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c2X") in (Taken_Total):
         break
-    if ("a3O") in (Taken_Total) and ("b3O") in (Taken_Total) and("c3O") in (Taken_Total): #2rd Col Down - O
+    # 2rd Col Down - O
+    if ("a3O") in (Taken_Total) and ("b3O") in (Taken_Total) and("c3O") in (Taken_Total):
         break
-    if ("a3X") in (Taken_Total) and ("b3X") in (Taken_Total) and ("c3X") in (Taken_Total): #3rd Col Down - X
+    # 3rd Col Down - X
+    if ("a3X") in (Taken_Total) and ("b3X") in (Taken_Total) and ("c3X") in (Taken_Total):
         break
-    if ("a1O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("c3O") in (Taken_Total): #Diagonally Left to Right - O
+    # Diagonally Left to Right - O
+    if ("a1O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("c3O") in (Taken_Total):
         break
-    if ("a1X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c3X") in (Taken_Total): #Diagonally Left to Right - X
+    # Diagonally Left to Right - X
+    if ("a1X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c3X") in (Taken_Total):
         break
-    if ("a3O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("c1O") in (Taken_Total): #Diagonally Right to Left - O
+    # Diagonally Right to Left - O
+    if ("a3O") in (Taken_Total) and ("b2O") in (Taken_Total) and ("c1O") in (Taken_Total):
         break
-    if ("a3X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c1X") in (Taken_Total): #Diagonally Right to Left - X
+    # Diagonally Right to Left - X
+    if ("a3X") in (Taken_Total) and ("b2X") in (Taken_Total) and ("c1X") in (Taken_Total):
         break
+       
     else:
-        #phew! The main code function
-        print("\n")
-        # Play Count - Switches Players & keeps track in case of draw
+        
+        print("\n")  # phew! The main code function
+       
+        '''Play Count - Switches Players & keeps track in case of draw'''
         (Play_Count) += 1
-        if (Play_Count) == 10: #checking for draw
+        if (Play_Count) == 10:  # checking for draw
             print("\n-- It's a draw! Everyone loses! --")
             print("\nFinal Grid:")
             break
-        #Switching players
+        # Switching players
         if (Play_Count)%2==0:
             Player = "X"
         else:
@@ -130,8 +157,8 @@ while True:
 print("\n")
 print_grid(grid)
         
-#Winner!
-if Play_Count != 10: #printing winning result
+# Winner!
+if Play_Count != 10:  # printing winning result
     print ("\n\n-- Game Over --")
     print ("-- ",(Player)," is the winner! --")
 print("\nThanks for playing!\n")
